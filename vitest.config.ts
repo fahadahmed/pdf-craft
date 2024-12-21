ba932d0ca71485a5e1a12e375d68905816823228
Fahad Ahmed <fahad.ahmed@me.com>
2024-12-21 19:08:25 +1100

    setup unit testing via vitest

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
