03b1ad94137b91bd9621867ef7070b6865651a5a
Fahad Ahmed <fahad.ahmed@me.com>
2024-12-23 15:34:52 +1100

    setup firebase client and server files

---
+++ src/env.d.ts
@@ -0,0 +1,12 @@
+interface ImportMetaEnv {
+  readonly PUBLIC_FIREBASE_API_KEY: string
+  readonly PUBLIC_FIREBASE_AUTH_DOMAIN: string
+  readonly PUBLIC_FIREBASE_STORAGE_BUCKET: string
+  readonly PUBLIC_FIREBASE_MESSENGER_ID: string
+  readonly PUBLIC_FIREBASE_APP_ID: string
+  readonly PUBLIC_FIREBASE_MEASUREMENT_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
