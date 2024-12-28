56b16022c820387c2347e5de2415dc1fc20ffd6a
Fahad Ahmed <fahad.ahmed@me.com>
2024-12-28 18:44:11 +0800

    #7- setup server rendering using node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from 'astro/config'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import react from '@astrojs/react'
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [react()],
   vite: {
     plugins: [vanillaExtractPlugin()],
