dfd2f7da13a174b676cbeed6a7ff5eef0cf87206
Fahad Ahmed <fahad.ahmed@me.com>
2025-01-14 05:38:29 +1100

    #7- updated the astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@
 import { defineConfig } from 'astro/config'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import react from '@astrojs/react'
-import node from '@astrojs/node'
+// import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  // output: 'server',
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
   integrations: [react()],
   vite: {
     plugins: [vanillaExtractPlugin()],
