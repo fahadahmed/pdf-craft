14dad80368d89a8374c1c3d9c8f2bea0ba287bf0
Fahad Ahmed <fahad.ahmed@me.com>
2025-01-14 05:43:28 +1100

    #7- updated astro config to include middleware support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,20 @@
 import { defineConfig } from 'astro/config'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import react from '@astrojs/react'
-// import node from '@astrojs/node'
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
-  // output: 'server',
-  // adapter: node({
-  //   mode: 'standalone',
-  // }),
+  output: 'server',
+  adapter: node({
+    mode: 'middleware',
+  }),
   integrations: [react()],
   vite: {
     plugins: [vanillaExtractPlugin()],
     optimizeDeps: {
       noDiscovery: true, // Disables dependency discovery
       include: [], // Ensure this is empty or undefined
-    },
-    ssr: {
-      target: 'node',
-      noExternal: ['react', 'react-dom'],
     },
   },
 })
