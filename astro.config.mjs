e052b82779713ead9107b00e0db2dcd134bedb9d
Fahad Ahmed <fahad.ahmed@me.com>
2025-01-16 11:40:10 +1100

    created a sample commit to check preview env builds (#12)
    * created a sample commit to check preview env builds
    
    * setup ssr"
    
    * updated pnpm install command in workflow file
    
    * downgraded firebase-admin to resolve CICD issue
    
    * changing the frameworks backend function location back to asia-east1
    
    * update to firebase.json file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,20 @@
 import { defineConfig } from 'astro/config'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import react from '@astrojs/react'
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'middleware',
+  }),
   integrations: [react()],
   vite: {
     plugins: [vanillaExtractPlugin()],
+    optimizeDeps: {
+      noDiscovery: true,
+      include: [],
+    },
   },
 })
