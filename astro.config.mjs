bc5b202aa73a511210cac02391c764e2d8f39bd4
Fahad Ahmed <fahad.ahmed@me.com>
2025-01-16 11:41:10 +1100

    Revert "created a sample commit to check preview env builds (#12)"
    This reverts commit e052b82779713ead9107b00e0db2dcd134bedb9d.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,11 @@
 import { defineConfig } from 'astro/config'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import react from '@astrojs/react'
-import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'middleware',
-  }),
   integrations: [react()],
   vite: {
     plugins: [vanillaExtractPlugin()],
-    optimizeDeps: {
-      noDiscovery: true,
-      include: [],
-    },
   },
 })
