dd6e08b4fc5c31c9d01607d99e784b9c150c8952
Fahad Ahmed <fahad.ahmed@me.com>
2025-01-18 13:10:10 +1100

    setup ssr for the astro app

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import react from '@astrojs/react'
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,4 +10,11 @@ export default defineConfig({
   vite: {
     plugins: [vanillaExtractPlugin()],
   },
+  server: {
+    port: 3000,
+  },
+  output: 'server',
+  adapter: node({
+    mode: 'middleware',
+  }),
 })
