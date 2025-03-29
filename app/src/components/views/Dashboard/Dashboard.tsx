e7a08d5ec9aef4885a2d34275c92bb707b0f1a0d
Fahad Ahmed <fahad.ahmed@me.com>
2025-03-29 11:16:58 +1100

    #8- slight copy update to create a PR

--- app/src/components/views/Dashboard/Dashboard.tsx
+++ app/src/components/views/Dashboard/Dashboard.tsx
@@ -3,7 +3,7 @@ import { actions } from 'astro:actions'
 export default function Dashboard() {
   return (
     <div>
-      <h1>Welcome to the Dashboard!</h1>
+      <h1>Welcome to the PDF-Craft</h1>
       <form action={actions.user.signOutUser} method="post">
         <button type="submit">Logout</button>
       </form>
