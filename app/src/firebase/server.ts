859e0def06d9d332bf6421c73900b835020d78e7
Fahad Ahmed <fahad.ahmed@me.com>
2025-04-25 18:33:16 +0530

    #35- fixing the production check

--- app/src/firebase/server.ts
+++ app/src/firebase/server.ts
@@ -11,7 +11,7 @@ let _app: ReturnType<typeof initializeApp> | undefined;
 let _auth: ReturnType<typeof getAuth> | undefined;
 
 export async function getFirebaseApp() {
-  const isProd = import.meta.env.NODE_ENV !== 'production';
+  const isProd = import.meta.env.NODE_ENV === 'production';
   const storageBucket = import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET;
 
   console.log(isProd, import.meta.env.PUBLIC_FIREBASE_SERVICEACCOUNT_KEY);
