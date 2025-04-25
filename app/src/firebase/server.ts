a8442a9ed7933e01a453cd548c8b6d011271c054
Fahad Ahmed <fahad.ahmed@me.com>
2025-04-25 08:40:44 +0530

    #35 fixed the issue of firebase app initialisation when localhost

--- app/src/firebase/server.ts
+++ app/src/firebase/server.ts
@@ -6,20 +6,25 @@ import {
 } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import type { ServiceAccount } from 'firebase-admin';
+import serviceAccount from '../serviceAccountKey.json';
 
 let _app: ReturnType<typeof initializeApp> | undefined;
 let _auth: ReturnType<typeof getAuth> | undefined;
 
 export function getFirebaseApp() {
+  const storageBucket = import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET;
+  const isProd = import.meta.env.NODE_ENV === 'production';
   if (!_app) {
-    const isProd = process.env.NODE_ENV === 'production';
-
-    _app = initializeApp({
-      credential: isProd
-        ? applicationDefault()
-        : cert(require('../serviceAccountKey.json') as ServiceAccount),
-      storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-    });
+    if (getApps().length === 0) {
+      _app = initializeApp({
+        credential: isProd
+          ? applicationDefault()
+          : cert(serviceAccount as ServiceAccount),
+        storageBucket,
+      });
+    } else {
+      _app = getApps()[0];
+    }
   }
   return _app;
 }
