7eeb0930687e9baca866416b734d2e396a7ec15d
Fahad Ahmed <fahad.ahmed@me.com>
2025-04-25 17:38:56 +0530

    #35- making the serviceAccountKey as optional import

--- app/src/firebase/server.ts
+++ app/src/firebase/server.ts
@@ -6,32 +6,38 @@ import {
 } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import type { ServiceAccount } from 'firebase-admin';
-import serviceAccount from '../serviceAccountKey.json';
 
 let _app: ReturnType<typeof initializeApp> | undefined;
 let _auth: ReturnType<typeof getAuth> | undefined;
 
-export function getFirebaseApp() {
-  const storageBucket = import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET;
+export async function getFirebaseApp() {
   const isProd = import.meta.env.NODE_ENV === 'production';
+  const storageBucket = import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET;
+
   if (!_app) {
     if (getApps().length === 0) {
+      const credential = isProd
+        ? applicationDefault()
+        : cert(
+            (await import('../serviceAccountKey.json'))
+              .default as ServiceAccount
+          );
+
       _app = initializeApp({
-        credential: isProd
-          ? applicationDefault()
-          : cert(serviceAccount as ServiceAccount),
+        credential,
         storageBucket,
       });
     } else {
       _app = getApps()[0];
     }
   }
+
   return _app;
 }
 
-export function getFirebaseAuth() {
+export async function getFirebaseAuth() {
   if (!_auth) {
-    _auth = getAuth(getFirebaseApp());
+    _auth = getAuth(await getFirebaseApp());
   }
   return _auth;
 }
